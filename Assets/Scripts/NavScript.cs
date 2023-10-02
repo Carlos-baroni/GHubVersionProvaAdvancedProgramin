96d70230d0043109f8b73ccf4f1ba99c4bd03826
Henrique Behr <146377684+Carlos-baroni@users.noreply.github.com>
2023-10-01 22:07:10 -0300

    TentativasFalhasDeMudançaDeCena

--- Assets/Scripts/NavScript.cs
+++ Assets/Scripts/NavScript.cs
@@ -2,22 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class NavScript : MonoBehaviour
 {
 
+  
+    public CharacterController enemyController;
+
     public Transform player;
     private NavMeshAgent agent;
-    
+    public Transform enemyTransform;
+
+
+    int speed;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        speed = 5;
+
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = player.position;
+        var position = player.position;
+        agent.destination = position;
     }
+
+    
 }
